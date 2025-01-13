2cdf3783c1241115cb5f12488531397947f04939
TheBoldReaper <reaperzenith2003@gmail.com>
2025-01-14 03:29:35 +0800

    Adds consultation page

--- src/pages/Consultation.tsx
+++ src/pages/Consultation.tsx
@@ -1,10 +1,21 @@
 import Navbar from "../components/Navbar";
+import './css/consultation.css'
+import consultationImg from '../assets/consultationperson.png'
 
 function Consultation() {
     return <>
         <Navbar />
         <div className="journey">Home {'>'} Consultation</div>
-        <main>
+        <div className="consultation-banner">
+            <div className="container-xl  ">
+                <div className="consultation-heading">
+                    <p>Need Expert Help?<br />Get Profressional<br />Assistance Here!</p>
+                    <button type="button" className="consultation-button">Get Help</button>
+                </div>
+                <img src={consultationImg} />
+            </div>
+        </div>
+        <main className="main-content">
 
         </main>
     </>
